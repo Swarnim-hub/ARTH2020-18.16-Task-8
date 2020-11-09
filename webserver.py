5aa6ba42957ba72d9d93e40dbaea2fa227d6dbc6
Swarnim-hub <68059131+Swarnim-hub@users.noreply.github.com>
2020-11-09 13:31:28 +0530

    Apache Webserver Configure

---
+++ webserver.py
@@ -0,0 +1,11 @@
+import os
+#os.system("espeak-ng install_h_t_t_p_d")
+os.system("yum install httpd")
+#os.system("espeak-ng opening_h_t_m_l_directory")
+os.system("cd /var/www/html/gedit myweb.html")
+#os.system("espeak-ng start__h_t_t_p_d_service")
+os.system("systemctl start httpd")
+os.system("ifconfig enp0s3")
+#os.system("espeak-ng please_copy_the_ip_address")
+#os.system("espeak-ng opening_firefox")
+os.system("firefox")
